e8adfd53c120ef99aebedc220d0fa5dec440aed3
jambrose0 <jambrose0518@gmail.com>
2022-01-12 21:04:17 -0500

    randomized turn order

--- assets/js/game.js
+++ assets/js/game.js
@@ -86,7 +86,14 @@ var fightOrSkip = function() {
 
 //fight function
 var fight = function(enemy) {
+   //keep track of who goes first
+   var isPlayerTurn = true;
+if (Math.random() > 0.5) {
+    isPlayerTurn = false;
+}
+
     while(playerInfo.health > 0 && enemy.health > 0) {
+       if (isPlayerTurn) {
         //ask player if they'd like to fight or skip
       if (fightOrSkip()) {
           break;
@@ -106,7 +113,8 @@ var fight = function(enemy) {
                    } else {
                     window.alert(enemy.name + " still has " + enemy.health + " health left.");
                 }
-
+                //player gets attacked first
+            } else {
                 //player damage
               var damage = randomNumber(enemy.attack - 3, enemy.attack);
                 playerInfo.health = Math.max(0, playerInfo.health - damage);
@@ -119,6 +127,9 @@ var fight = function(enemy) {
                 }    else {
                     window.alert(playerInfo.name + " still has " + playerInfo.health + " health left.");
                 }
+            }
+            //switch turn order
+            isPlayerTurn = !isPlayerTurn;
               }
             }; 
  //at start of game          
